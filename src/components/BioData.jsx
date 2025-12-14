cb6617abc14d1b8e6ae9c900550bb284bcb2b766
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2025-12-15 03:51:02 +0600

    Update BioData.jsx

--- src/components/BioData.jsx
+++ src/components/BioData.jsx
@@ -1,64 +1,53 @@
-const BioData = (props) => {
+export default function BioData(props) {
     // let age = 35;
-    // console.log(props, "props");
+    console.log(props);
+
     return (
         <div className="bio-data">
             <div className="personal-info">
                 <h2>BioData of {props.name}</h2>
                 {/* <h3>{age}</h3>  // JSX er vitore JS code likhle = Second Bracket {} dibo */}
                 <p>
-                    <strong>Email: </strong>{props.email}
+                    <strong>Email:</strong> {props.email}
                 </p>
                 <p>
-                    <strong>Phone: </strong>{props.phone}
+                    <strong>Phone:</strong> {props.phone}
                 </p>
                 <p>
-                    <strong>Github: </strong>{props.github}
+                    <strong>Github:</strong> {props.github}
                 </p>
             </div>
+
             <div className="skills">
                 <h2>My Skills:</h2>
                 <ul>
                     {
-                        props?.skills?.map(skill => (
-                            <li key={skill}>{skill}</li>
+                        props.skills.map(skill => (
+                            <li>{skill}</li>
                         ))
                     }
                 </ul>
             </div>
+
             <div className="interests">
                 <h2>My Interests:</h2>
                 <ul>
-                    {
-                        props?.interests?.map(interest => (
-                            <li key={interest}>{interest}</li>
-                        ))
-                    }
+                    {props.interests.map(interest => (
+                        <li>{interest}</li>
+                    ))}
                 </ul>
             </div>
+
             <div className="social-links">
                 <h2>My Social Medias:</h2>
                 <ul>
-                    {
-                        props?.socialLinks?.map(socialLink => (
-                            <li><strong>{socialLink.platformName}:</strong>
-                                {socialLink.handle}
-                            </li>
-                        ))
+                    {props.socialLinks.map(socialLink => (
+                        <li><strong>{socialLink.platformName}: </strong>{socialLink.handle}</li>
+                    ))
                     }
                 </ul>
             </div>
+            <hr /><hr />
         </div>
-    );
-};
-
-export default BioData;
-
-/**
- * A component must fulfil 3 criteria:
- *   1) A component must be a function
- *   2) That function should return "something"
- *   3) That "something" should be some html-ish code (jsx)
- */
-
-// Props is actually parameter
+    )
+}
