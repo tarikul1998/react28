4d4c0654144dd85945f251d8a10be57c83530ac8
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2025-12-15 04:05:55 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,3 +53,60 @@ export default App
  */
 
 // Props is actually parameter
+
+
+import './App.css'
+import BioData from './components/BioData'
+// Suppose, array akaare api(backend) theke data ashche, UI te dekhabo
+const bioData = [
+  {
+    name: "SR Setu",
+    email: "srsetu@gmail.com",
+    phone: "+245456452132499",
+    github: "github.com/srsetu",
+    skills: ["JS", "WP", "CSS", "REACT", "Next"],
+    interests: ["Chess", "Football", "Entrepreneurship"],
+    socialLinks: [
+      { platformName: "FB", handle: "fb/srsetu" },
+      { platformName: "Twitter", handle: "x/srsetu" },
+      { platformName: "LinkedIn", handle: "linkedin/in/srsetu" }
+    ]
+  },
+  {
+    name: "Mahir Asief",
+    email: "asiefmahir1@gmail.com",
+    phone: "+245456452132452",
+    github: "github.com/asiefmahit",
+    skills: ["JS", "REACT", "Next"],
+    interests: ["Chess", "Football"],
+    socialLinks: [
+      { platformName: "FB", handle: "fb/srsetu" },
+      { platformName: "Twitter", handle: "x/srsetu" },
+      { platformName: "LinkedIn", handle: "linkedin/in/srsetu" }
+    ]
+  },
+]
+
+
+function App() {
+
+  return (
+    <>
+      {bioData.map((singleBioData, index) => (
+        <div key={index}>
+          <BioData
+            name={singleBioData.name}
+            email={singleBioData.email}
+            phone={singleBioData.phone}
+            skills={singleBioData.skills}
+            interests={singleBioData.interests}
+            socialLinks={singleBioData.socialLinks}
+          />
+          <hr /><hr />
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
