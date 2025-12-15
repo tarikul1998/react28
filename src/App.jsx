e89d78a520988be1ff933b6c318b8f2fdfd61f41
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2025-12-15 15:29:41 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,27 @@ import BioData from './components/BioData'
 function App() {
   // console.log("I am invoked");
   return (
-    <>
-      <BioData
-        name="SR Setu"
-        email="srsetu@gmail.com"
-        phone="+245456452132452"
-        github="github.com/srsetu"
-        skills={["JS", "WP", "CSS", "REACT", "Next"]}
-        interests={["Chess", "Football", "Entrepreneurship"]}
-        socialLinks={[
-          { platformName: "FB", handle: "fb/srsetu" },
-          { platformName: "Twitter", handle: "x/srsetu" },
-          { platformName: "LinkedIn", handle: "linkedin/in/srsetu" }
-        ]}
-      />
-      <BioData
-        name="Mahir Asief"
-        email="asiefmahir1@gmail.com"
-        phone="+43543543545"
-        github="github.com/asiefmahir"
-        skills={["JS", "REACT", "Next"]}
-        interests={["Chess", "Football"]}
-        socialLinks={[
-          { platformName: "FB", handle: "fb/asiefmahir" },
-          { platformName: "Twitter", handle: "x/asiefmahir" },
-          {
-            platformName: "LinkedIn",
-            handle: "linkedin/in/asiefmahir",
-          },
-        ]}
-      />
-    </>
+    <BioData
+      name="SR Setu"
+      email="srsetu@gmail.com"
+      phone="+245456452132452"
+      github="github.com/srsetu"
+      // Amra j skills pathabo attribute er moto kore equal diye,
+      // Since return er moddhe sob html(jsx code), Amra jani jsx er vitore JS code likhle (Eikhane Array) = {} er vitore likhte hobe
+      // Object er moddhe array pathacchi emon nah btw
+      skills={["JS", "WP", "CSS", "REACT", "Next"]}
+      interests={["Chess", "Football", "Entrepreneurship"]}
+      socialLinks={[
+        { platformName: "FB", handle: "fb/srsetu" },
+        { platformName: "Twitter", handle: "x/srsetu" },
+        { platformName: "LinkedIn", handle: "linkedin/in/srsetu" }
+      ]}
+    />
   )
 }
 
 export default App
+
 
 
 // JSX = JavaScript XML
